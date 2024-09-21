3209a0faa1790a357c442bad1f29fdb679d56e5c
ralaurent <ral.laurent1@gmail.com>
2024-09-21 03:02:02 -0400

    adding secret

--- next.config.js
+++ next.config.js
@@ -22,7 +22,8 @@ const nextConfig = {
       FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
       FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
       FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-      FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+      FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     },
     webpack: (config) => {
       config.module.rules.push({
