99df330d24edc21347779dee0c1663d6714352f0
ralaurent <ral.laurent1@gmail.com>
2024-09-21 02:47:59 -0400

    fixed dependency issue

--- src/components/dojo/components/actions.tsx
+++ src/components/dojo/components/actions.tsx
@@ -32,7 +32,11 @@ export function Actions() {
   const { data: session } = useSession()
 
   const toggleRLHF = () => {
-    if(!session?.user) return setOpenAuth(true) 
+    if(!session?.user) {
+      setIsOpen(false)
+      setOpenAuth(true) 
+      return 
+    }
     setIsRLHFEnabled(!isRLHFEnabled)
     setIsOpen(false)
   }
@@ -86,31 +90,6 @@ export function Actions() {
           </DialogFooter>
         </DialogContent>
       </Dialog>
-      {/* <AlertDialog open={showDeleteDialog} onOpenChange={setShowDeleteDialog}>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you sure absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This preset will no longer be
-              accessible by you or others you&apos;ve shared it with.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <Button
-              variant="destructive"
-              onClick={() => {
-                setShowDeleteDialog(false)
-                toast({
-                  description: "This preset has been deleted.",
-                })
-              }}
-            >
-              Delete
-            </Button>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog> */}
     </>
   )
 }
